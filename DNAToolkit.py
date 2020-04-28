b75407634f28f4c15354ca5792ba0b15d20810ae
Nazar <nazar.semenyuck@yandex.ua>
2020-04-28 14:29:23 +0300

    Aminoacid sequences, codon usage

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,4 +1,6 @@
-from structures import DNA_nucleotides
+from collections import Counter
+
+from structures import DNA_nucleotides, DNA_codons
 
 
 def validate_seq(dna_seq: str) -> [bool, str]:
@@ -53,3 +55,23 @@ def gc_content_subsec(seq: str, k=10) -> list:
         subseq = seq[i:i + k]
         res.append(gc_content(subseq))
     return res
+
+
+def translate_seq(dna_seq: str, init_pos=0) -> DNA_codons:
+    """Translates a DNA sequence into an aminoacid sequence"""
+
+    return [DNA_codons[dna_seq[pos:pos + 3]] for pos in range(init_pos, len(dna_seq) - 2, 3)]
+
+
+def codon_usage(dna_seq: str, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    
+    tmp_list = []
+    for i in range(0, len(dna_seq) - 2, 3):
+        if DNA_codons[dna_seq[i:i + 3]] is aminoacid:
+            tmp_list.append(dna_seq[i:i + 3])
+    freq_dict = dict(Counter(tmp_list))
+    total = sum(freq_dict.values())
+    for seq in freq_dict:
+        freq_dict[seq] = round(freq_dict[seq] / total, 2)
+    return freq_dict
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utilities import color_output
 
 if __name__ == '__main__':
     dna_seq = ''.join([random.choice(DNA_nucleotides)
-                       for nuc in range(50)])
+                       for nuc in range(80)])
     dna_seq = validate_seq(dna_seq)
     print(f"[1] Sequence length: {len(dna_seq)} \n"
           f"[2] Nucleotide frequency: {color_output(str(count_nuc_frequency(dna_seq)))} \n"
@@ -16,4 +16,6 @@ if __name__ == '__main__':
           f"\t3' {color_output(reverse_complement(dna_seq)[::-1])} 5' [Complement]\n"
           f"\t5' {color_output(reverse_complement(dna_seq))} 3' [Reverse Complement]\n"
           f"[5] GC content: {gc_content(dna_seq)}% \n"
-          f"[6] GC content in subsection k=20: {gc_content_subsec(dna_seq, k=20)}")
+          f"[6] GC content in subsection k=5: {gc_content_subsec(dna_seq, k=5)} \n"
+          f"[7] Aminoacid Sequence from DNA string: {translate_seq(dna_seq, init_pos=0)} \n"
+          f"[8] Codon frequency (R): {codon_usage(dna_seq, aminoacid='R')}")
